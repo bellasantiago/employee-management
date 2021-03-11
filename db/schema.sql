113509ab3d7871b774c37133f5d9450293770e4c
Bella Santiago <bellasantiago.adl@gmail.com>
2021-03-11 12:10:14 +1030

    Setup schema done

--- db/schema.sql
+++ db/schema.sql
@@ -5,12 +5,21 @@ USE staff_db;
 
 CREATE TABLE employee(
     ID INTEGER PRIMARY KEY AUTO_INCREMENT;
+    PRIMARY KEY (id),
+    first_name VARCHAR(30),
+    last_name VARCHAR (30),
+    role_id INTEGER,
+    manager_id INTEGER;
 );
 
 CREATE TABLE role(
     ID INTEGER PRIMARY KEY AUTO_INCREMENT;
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INTEGER;
 );
 
 CREATE TABLE department(
     ID INTEGER PRIMARY KEY AUTO_INCREMENT;
+    name VARCHAR(30);
 );
