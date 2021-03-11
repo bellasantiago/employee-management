d6aa6b3f38553a2c0ce5aa0ba7f5f38449ab1d6c
Bella Santiago <bellasantiago.adl@gmail.com>
2021-03-11 12:17:39 +1030

    Added basic seeds schema

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE staff_db;
+
+INSERT INTO department (department)
+VALUES ('Sales'), ('Engineering'), ('Finance'), ('Legal');
+
+INSERT INTO role ( title, salary, department_id)
+VALUES ('Sales Manager', '90000', '1'), ('Sales', '45000', '1');
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('John', 'Appleseed', 1, NULL), ('Mary', 'Appleseed', 2, 1);
