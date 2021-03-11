a3cb718d9b6f8292c7788584a4a822ff1db31a5d
Bella Santiago <bellasantiago.adl@gmail.com>
2021-03-11 12:42:22 +1030

    First prompt

--- server.js
+++ server.js
@@ -2,3 +2,49 @@ const connection = require("./connection");
 const { prompt } = require("inquirer");
 const db = require("./db");
 require("console.table");
+
+console.log("Let's have a look at the company's staff!")
+
+function mainList() {
+    prompt([{
+            type: "list",
+            name: "userChoices",
+            message: "Select what you'd like to do:",
+            choices: [
+                "+Departments",
+                "+Roles",
+                "+Employees",
+                "Departments",
+                "Roles",
+                "Employees",
+                "Update",
+                "Done!"
+            ]
+        }]).then(result => {
+        switch(userAnswers.userChoices) {
+            case "+Departments":
+                addDepartments();
+                break;
+            case "+Roles":
+                addRoles();
+                break;
+            case "+Employees":
+                addEmployees();
+                break;
+            case "Departments":
+                allDepartments();
+                break;
+            case "Roles":
+                addRoles();
+                break;
+            case "Employees":
+                employees();
+                break;
+            case "Update":
+                update();
+                break;
+            default:
+                quit();
+        }
+    })
+}
