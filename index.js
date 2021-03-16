933fcb2da9976c0880026169837736ee92afda72
Bella Santiago <bellasantiago.adl@gmail.com>
2021-03-16 18:34:14 +1030

    Add role - department pending

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ const connectMYSQL = mysql.createConnection({
     user: "root",
     password: "mysql",
     database: "staff_db"
-  });
+});
 
-  connectMYSQL.connect((err) => {
-      if (err) throw err;
-      mainList();
-  })
+connectMYSQL.connect((err) => {
+    if (err) throw err;
+    mainList();
+})
 console.log("\n");
 console.log("\n");
 console.log("LET'S HAVE A LOOK AT THE COMPANY'S STAFF!");
@@ -24,21 +24,21 @@ console.log("\n");
 
 function mainList() {
     prompt([{
-            type: "list",
-            name: "userChoices",
-            message: "Select what you'd like to do:",
-            choices: [
-                "Add Departments",
-                "Add Roles",
-                "Add Employees",
-                "See Departments",
-                "See Roles",
-                "See Employees",
-                "Employee Role Update",
-                "Quit"
-            ]
-        }]).then(result => {
-        switch(result.userChoices) {
+        type: "list",
+        name: "userChoices",
+        message: "Select what you'd like to do:",
+        choices: [
+            "Add Departments",
+            "Add Roles",
+            "Add Employees",
+            "See Departments",
+            "See Roles",
+            "See Employees",
+            "Employee Role Update",
+            "Quit"
+        ]
+    }]).then(result => {
+        switch (result.userChoices) {
             case "Add Departments":
                 addDepartments();
                 break;
@@ -71,49 +71,46 @@ function addDepartments() {
             message: "Please enter department name."
         }
     ]).then((answer) => {
-            const query_str = "INSERT INTO department (name) VALUES (?);";
-            connectMYSQL.query(query_str, [answer.department], (err, res) => {
-                if (err) throw err;
-            });
-        })
-    .then(() => mainList())
+        const query_str = "INSERT INTO department (name) VALUES (?);";
+        connectMYSQL.query(query_str, [answer.department], (err, res) => {
+            if (err) throw err;
+        });
+    })
+        .then(() => mainList())
 
 };
 
-var dptChoices = []
-
 function addRoles(roles) {
-    const departmentChoice = () => {
-        let query_str = "SELECT department.id, department.name FROM department;";
-        connectMYSQL.query(query_str, (err, res) => {
-            if (err) throw err;
-            let dptChoices = res.map(a => a.department);
-            console.log(dptChoices);
-            })
-    };
-    prompt([
-        {
-            name: "title",
-            type: "input",
-            message: "Please enter role's title name."
-        },{
-            name: "salary",
-            type: "input",
-            message: "Please enter role's salary."
-        },{
-            name: "departmentID",
-            type: "list",
-            message: "Please enter the department this role belongs to.",
-            choices: dptChoices
-        },
+    let query_str = "SELECT department.id, department.name FROM department;";
+    connectMYSQL.query(query_str, (err, res) => {
+        if (err) throw err;
+        let dptChoices = res.map(a => a.department);
+        console.log(dptChoices);
+        prompt([
+            {
+                name: "title",
+                type: "input",
+                message: "Please enter role's title name."
+            }, {
+                name: "salary",
+                type: "input",
+                message: "Please enter role's salary."
+            }, {
+                name: "departmentID",
+                type: "list",
+                message: "Please enter the department this role belongs to.",
+                choices: ["dptChoices"]
+            },
 
-    ]).then((answer) => {
+        ]).then((answer) => {
             const query_str = "INSERT INTO role (title, salary, department_id) VALUES (?,?,?);";
             connectMYSQL.query(query_str, [answer.title, answer.salary, answer.departmentID], (err, res) => {
                 if (err) throw err;
             });
         })
-    .then(() => mainList())
+            .then(() => mainList())
+    })
+
 }
 
 function addEmployees() {
@@ -121,24 +118,24 @@ function addEmployees() {
         {
             name: "first_name",
             message: "Please enter employee's first name."
-        },{
+        }, {
             name: "last_name",
             message: "Please enter employee's last name."
-        },{
+        }, {
             name: "role_id",
             message: roleID
-        },{
+        }, {
             name: "manager_id",
             message: managerID
         },
     ]).then((answer) => {
         const query_str = "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?);";
-            connectMYSQL.query(query_str, [answer.first_name, answer.first_name, answer.role_id, answer.manager_id], (err, res) => {
-                if (err) throw err;
-            });
-        })
-    
-    .then(() => mainList())
+        connectMYSQL.query(query_str, [answer.first_name, answer.first_name, answer.role_id, answer.manager_id], (err, res) => {
+            if (err) throw err;
+        });
+    })
+
+        .then(() => mainList())
 }
 
 function allDepartments(department) {
@@ -164,13 +161,13 @@ function allRoles(role) {
             let role = rows;
             console.table("\n");
             console.table(role)
-            
+
         }
         mainList();
     })
 }
 
-function allEmployees(employee){
+function allEmployees(employee) {
     var query_str = "SELECT employee.id, employee.first_name, employee.last_name, employee.role_id, employee.manager_id FROM employee;";
     var query = connectMYSQL.query(query_str, function (err, rows) {
         if (err) {
@@ -179,7 +176,7 @@ function allEmployees(employee){
             let employee = rows;
             console.table("\n");
             console.table(employee)
-            
+
         }
         mainList();
     })
